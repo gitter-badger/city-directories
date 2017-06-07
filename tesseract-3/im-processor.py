7e5efb75036a7dad8d4b11a927922e6c2359a2ce
Nicholas Wolf <nickwolf49@gmail.com>
2017-06-07 11:54:36 -0400

    Fixing sysargv problems

--- tesseract-3/im-processor.py
+++ tesseract-3/im-processor.py
@@ -3,11 +3,11 @@
 import os, sys
 import time
 
-cleanerpath = sys.argv[1].replace('"','').replace("'","")
+cleanerpath = "'" + sys.argv[1].replace('"','').replace("'","") + "'"
 	#Input image folder name:
-imagefolder = sys.argv[2].replace('"','').replace("'","").rstrip('/') + '/'
+imagefolder = sys.argv[2].replace('"','').replace("'","").rstrip('/')  + "/"
     #Output folder name:
-outfolder = sys.argv[3].replace('"','').replace("'","").rstrip('/') + '/'
+outfolder = "'" + sys.argv[3].replace('"','').replace("'","").rstrip('/') + "/'"
 filelist = [files for root, dirs, files in os.walk(imagefolder)][0]
 
 start = time.time()
@@ -15,7 +15,8 @@ start = time.time()
 def runIM(file, out):
     try:
         command = ' '.join(["bash", cleanerpath, "-g", "-e none", "-f 25", "-o 10", "-T -b white", 
-                   "'"+imagefolder + file+"'", "'"+out + file.replace('.tif','') + "-processed.tif'"])
+                   "'"+imagefolder + file+"'", out.rstrip("'") + file.replace(".tif","") + "-processed.tif'"])
+        print(command)
         os.system(command)
     except:
         print('Preprocessing failed on ', file)
